61c0cfa3966d6009dcfbfe788c6c1c06456d4eb0
pranav <sonawanepranav19@gmail.com>
2023-03-18 23:47:43 +0530

    added sass files and created a css folder

---
+++ index.js
@@ -0,0 +1,9 @@
+// NAVBAR SCROLL
+const navbar = document.querySelector('.navbar');
+window.onscroll = () => {
+    if (window.scrollY > 300) {
+        navbar.classList.add('nav-active');
+    } else {
+        navbar.classList.remove('nav-active');
+    }
+};
